e2cdb23316168088af1e68623f5c0cb88ec8ab34
farazzhh <havaee.faraz@gmail.com>
2021-10-16 17:36:53 +0330

    first commit

--- components/Header.js
+++ components/Header.js
@@ -3,16 +3,26 @@ import Image from "next/image";
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
-
       <div className="flex justify-between max-w-6xl">
         {/* left */}
-        <div className="relative h-24 w-24">
-                  <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
+        <div className="relative hidden lg:inline-grid h-24 w-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative lg:hidden w-10 h-10 flex-shrink-0 cur">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
         </div>
         {/* middle */}
 
-              <h1>hello</h1>
+        <h1>hello</h1>
         {/* Right */}
       </div>
     </div>
